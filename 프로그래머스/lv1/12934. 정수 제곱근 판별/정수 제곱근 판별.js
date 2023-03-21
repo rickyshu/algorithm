3bb6d4f6a1ab4fbda56b7d505185b9665675b503
이호인 <104845427+rickyshu@users.noreply.github.com>
2023-03-21 14:35:14 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,8 @@
+function solution(n) {
+            let ans=-1;
+          if(parseInt(Math.sqrt(n))===Math.sqrt(n)){
+            let num=Math.sqrt(n)
+            return (num+1)*(num+1);
+          }
+          return ans;
+}
